4c9f1fbce152295a66a24ea70f7a692b02bf486a
nitou-kanazawa <121334451+nitou-kanazawa@users.noreply.github.com>
2025-04-17 01:05:08 +0900

    .editorcongigの修正

--- NTween/Internal/Error.cs
+++ NTween/Internal/Error.cs
@@ -3,26 +3,39 @@ using System.Runtime.CompilerServices;
 
 namespace NTween {
 
-    /// <summary>
-    /// 
-    /// </summary>
-    internal static class Error {
+	/// <summary>
+	///
+	/// </summary>
+	internal static class Error {
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
-        public static void ArgumentNull(string message) {
-            throw new ArgumentNullException(message);
-        }
 
-        public static void TweenAlreadyRegistered() {
-            throw new InvalidOperationException("Tween is already registered.");
-        }
 
-        public static void TweenNotExists() {
-            throw new InvalidOperationException("Tween has been destroyed or no longer exists.");
-        }
 
-        public static void MotionHasBeenCanceledOrCompleted() {
-            throw new InvalidOperationException("Tween has already been canceled or completed.");
-        }
-    }
+		public static void ArgumentNull(string message) {
+			throw new ArgumentNullException(message);
+		}
+
+
+		public static void TweenAlreadyRegistered() {
+			throw new InvalidOperationException("Tween is already registered.");
+		}
+
+		public static void TweenNotExists() {
+			throw new InvalidOperationException("Tween has been destroyed or no longer exists.");
+		}
+
+		public static void MotionHasBeenCanceledOrCompleted() {
+			throw new InvalidOperationException("Tween has already been canceled or completed.");
+		}
+	}
 
 }
--- NTween/TweenStorage.cs
+++ NTween/TweenStorage.cs
@@ -3,55 +3,69 @@ using System.Collections.Generic;
 
 namespace NTween {
 
-    internal sealed class TweenStorage {
-
-        private readonly List<ITween> _tweens = new();
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public IEnumerable<ITween> Tweens => _tweens;
+	internal sealed class TweenStorage {
 
 
-        /// ----------------------------------------------------------------------------
-        // Public Method
 
-        internal TweenStorage() {
+		private readonly List<ITween> _tweens = new();
 
-        }
+		/// <summary>
+		///
+		/// </summary>
+		public IEnumerable<ITween> Tweens => _tweens;
 
-        public TweenHandle Register(ITween tween) {
-            if (tween.IsRegistered)
-                Error.TweenAlreadyRegistered();
 
-            // 対象の登録
-            var index = _tweens.Count;
-            tween.Index = index;
-            _tweens.Add(tween);
+		/// ----------------------------------------------------------------------------
+		// Public Method
 
-            return new TweenHandle(index);
-        }
+		internal TweenStorage() {
 
-        public void Unregister(TweenHandle handle) {
-            int removeIndex = handle.Index;
-            if (removeIndex < 0 || removeIndex >= _tweens.Count)
-                return;
+		}
 
-            var lastIndex = _tweens.Count - 1;
+		public TweenHandle Register(ITween tween) {
+			if (tween.IsRegistered)
+				Error.TweenAlreadyRegistered();
 
-            // 対象の登録解除
-            var removedTween = _tweens[removeIndex];
-            removedTween.Index = -1;
+			// 対象の登録
+			var index = _tweens.Count;
+			tween.Index = index;
+			_tweens.Add(tween);
 
-            // 末尾要素の移動
-            if (removeIndex != lastIndex) {
-                var lastTween = _tweens[lastIndex];
-                _tweens[removeIndex] = lastTween;
-                lastTween.Index = removeIndex;
-            }
-            // リスト更新
-            _tweens.RemoveAt(lastIndex);
-        }
-    }
+			return new TweenHandle(index);
+		}
+
+		public void Unregister(TweenHandle handle) {
+			int removeIndex = handle.Index;
+			if (removeIndex < 0 || removeIndex >= _tweens.Count)
+				return;
+
+			var lastIndex = _tweens.Count - 1;
+
+			// 対象の登録解除
+			var removedTween = _tweens[removeIndex];
+			removedTween.Index = -1;
+
+			// 末尾要素の移動
+			if (removeIndex != lastIndex) {
+				var lastTween = _tweens[lastIndex];
+				_tweens[removeIndex] = lastTween;
+				lastTween.Index = removeIndex;
+			}
+			// リスト更新
+			_tweens.RemoveAt(lastIndex);
+		}
+
+		public override bool Equals(object? obj) {
+			return base.Equals(obj);
+		}
+
+		public override int GetHashCode() {
+			return base.GetHashCode();
+		}
+
+		public override string? ToString() {
+			return base.ToString();
+		}
+	}
 
 }
